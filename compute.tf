45a66319cd4cb39e30283c5c09b8a139035c5d95
admin <daniel_brito@epam.com>
2026-01-13 17:31:51 -0300

    formatted compute.tf

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
-  ami           = "ami-0c55b159cbfafe1f0" # Exemplo: Amazon Linux 2, ajuste conforme necessário
-  instance_type = "t2.micro"
-  subnet_id     = data.terraform_remote_state.base_infra.outputs.public_subnet_id
+  ami                    = "ami-0c55b159cbfafe1f0" # Exemplo: Amazon Linux 2, ajuste conforme necessário
+  instance_type          = "t2.micro"
+  subnet_id              = data.terraform_remote_state.base_infra.outputs.public_subnet_id
   vpc_security_group_ids = [data.terraform_remote_state.base_infra.outputs.security_group_id]
 
   tags = {
